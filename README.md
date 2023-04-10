69862bce001d125bba29e4758d9d090fe949a482
Professional-Websites <89712320+Professional-Websites@users.noreply.github.com>
2023-04-11 03:28:24 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,94 @@
-# AUTOAI
+<p align="center">
+  <img src="https://raw.githubusercontent.com/reworkd/AgentGPT/main/public/banner.png?token=GHSAT0AAAAAAB7JND3U3VGGF3UYYHGYO4RAZBSDJAQ" height="300"/>
+</p>
+<p align="center">
+  <em>🤖 Assemble, configure, and deploy autonomous AI Agents in your browser. 🤖 </em>
+</p>
+<p align="center">
+    <img alt="Node version" src="https://img.shields.io/static/v1?label=node&message=%20%3E=16.0.0&logo=node.js&color=2334D058" />
+</p>
+
+<p align="center">
+<a href="https://agentgpt.reworkd.ai">🔗 Short link</a>
+<span>&nbsp;&nbsp;•&nbsp;&nbsp;</span>
+<a href="#-getting-started">🤝 Contribute</a>
+<span>&nbsp;&nbsp;•&nbsp;&nbsp;</span>
+<a href="https://twitter.com/asimdotshrestha/status/1644883727707959296">🐦 Twitter</a>
+</p>
+
+---
+
+AgentGPT allows you to configure and deploy Autonomous AI agents.
+Name your own custom AI and have it embark on any goal imaginable.
+It will attempt to reach the goal by thinking of tasks to do, executing them, and learning from the results 🚀.
+
+## 🎉 Features
+
+This platform is currently in beta, we are currently working on:
+
+- Long term memory 🧠
+- Web browsing 🌐
+- Interaction with websites and people 👨‍👩‍👦
+
+More Coming soon...
+
+## 🚀 Tech Stack
+
+- ✅ **Bootstrapping**: [create-t3-app](https://create.t3.gg).
+- ✅ **Framework**: [Nextjs 13 + Typescript](https://nextjs.org/).
+- ✅ **Auth**: [Next-Auth.js](https://next-auth.js.org)
+- ✅ **ORM**: [Prisma](https://prisma.io).
+- ✅ **Database**: [Supabase](https://supabase.com/).
+- ✅ **Styling**: [TailwindCSS + HeadlessUI](https://tailwindcss.com).
+- ✅ **Typescript Schema Validation**: [Zod](https://github.com/colinhacks/zod).
+- ✅ **End-to-end typesafe API**: [tRPC](https://trpc.io/).
+
+## 👨‍🚀 Getting Started
+
+> 🚧 You will need [Nodejs +16 (LTS recommended)](https://nodejs.org/en/) installed.
+
+1. Fork this project:
+
+- [Click here](https://github.com/reworkd/AgentGPT/fork).
+
+2. Clone the repository:
+
+```bash
+git clone git@github.com:YOU_USER/AgentGPT.git
+```
+
+3. Install dependencies:
+
+```bash
+npm install
+```
+
+4. Create a **.env** file with the following content:
+
+> 🚧 The environment variables must match the following [schema](https://github.com/reworkd/AgentGPT/blob/main/src/env/schema.mjs).
+
+```bash
+# Deployment Environment:
+NODE_ENV=development
+
+# Next Auth config:
+# Generate a secret with `openssl rand -base64 32`
+NEXTAUTH_SECRET=changeme
+NEXTAUTH_URL=http://localhost:3000
+
+# Prisma
+DATABASE_URL=file:./db.sqlite
+
+# External APIs:
+OPENAI_API_KEY=changeme
+```
+
+5. Ready 🥳, now run:
+
+```bash
+# Create database migrations
+npx prisma db push
+
+# Run the project:
+npm run dev
+```
---
+++ src/pages/index.tsx
@@ -0,0 +1,164 @@
+import { type NextPage } from "next";
+import Badge from "../components/Badge";
+import DefaultLayout from "../layout/default";
+import React, { useEffect } from "react";
+import type { Message } from "../components/ChatWindow";
+import ChatWindow from "../components/ChatWindow";
+import Drawer from "../components/Drawer";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import { FaRobot, FaStar } from "react-icons/fa";
+import PopIn from "../components/motions/popin";
+import { VscLoading } from "react-icons/vsc";
+import AutonomousAgent from "../components/AutonomousAgent";
+import Expand from "../components/motions/expand";
+import Dialog from "../components/Dialog";
+
+const Home: NextPage = () => {
+  const [name, setName] = React.useState<string>("");
+  const [goalInput, setGoalInput] = React.useState<string>("");
+  const [agent, setAgent] = React.useState<AutonomousAgent | null>(null);
+  const [stoppingAgent, setStoppingAgent] = React.useState(false);
+
+  const [messages, setMessages] = React.useState<Message[]>([]);
+
+  const [showModal, setShowModal] = React.useState(false);
+
+  useEffect(() => {
+    const key = "agentgpt-modal-opened";
+    const savedModalData = localStorage.getItem(key);
+    if (savedModalData == null) {
+      setTimeout(() => {
+        setShowModal(true);
+      }, 1700);
+    }
+    localStorage.setItem(key, JSON.stringify(true));
+  }, []);
+
+  useEffect(() => {
+    if (agent == null) {
+      setStoppingAgent(false);
+    }
+  }, [agent]);
+
+  const handleNewGoal = () => {
+    const addMessage = (message: Message) =>
+      setMessages((prev) => [...prev, message]);
+    const agent = new AutonomousAgent(name, goalInput, addMessage, () =>
+      setAgent(null)
+    );
+    setAgent(agent);
+    agent.run().then(console.log).catch(console.error);
+  };
+
+  const handleStopAgent = () => {
+    setStoppingAgent(true);
+    agent?.stopAgent();
+  };
+
+  return (
+    <DefaultLayout>
+      <Dialog showModal={showModal} setShowModal={setShowModal} />
+      <main className="flex h-screen w-screen flex-row">
+        <Drawer handleHelp={() => setShowModal(true)} />
+        <div
+          id="content"
+          className="z-10 flex h-screen w-full items-center justify-center p-2 px-2 sm:px-4 md:px-10"
+        >
+          <div
+            id="layout"
+            className="flex h-full w-full max-w-screen-lg flex-col items-center justify-between gap-3 py-5 md:justify-center"
+          >
+            <div
+              id="title"
+              className="relative flex flex-col items-center font-mono"
+            >
+              <div className="flex flex-row items-start shadow-2xl">
+                <span className="text-4xl font-bold text-[#C0C0C0] xs:text-5xl sm:text-6xl">
+                  Agent
+                </span>
+                <span className="text-4xl font-bold text-white xs:text-5xl sm:text-6xl">
+                  GPT
+                </span>
+                <PopIn delay={0.5}>
+                  <Badge>Beta 🚀</Badge>
+                </PopIn>
+              </div>
+              <div className="mt-1 text-center font-mono text-[0.7em] font-bold text-white">
+                Assemble, configure, and deploy autonomous AI Agents in your
+                browser.
+              </div>
+            </div>
+
+            <Expand className="w-full">
+              <ChatWindow className="mt-4" messages={messages} />
+            </Expand>
+
+            <div className="mt-10 flex w-full flex-col gap-2">
+              <Input
+                left={
+                  <>
+                    <FaRobot />
+                    <span className="ml-2">Name:</span>
+                  </>
+                }
+                value={name}
+                disabled={agent != null}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="AgentGPT"
+              />
+
+              <Input
+                left={
+                  <>
+                    <FaStar />
+                    <span className="ml-2">Goal:</span>
+                  </>
+                }
+                disabled={agent != null}
+                value={goalInput}
+                onChange={(e) => setGoalInput(e.target.value)}
+                placeholder="Make the world a better place."
+              />
+            </div>
+
+            <div className="flex gap-2">
+              <Button
+                disabled={agent != null || name === "" || goalInput === ""}
+                onClick={handleNewGoal}
+                className="mt-10"
+              >
+                {agent == null ? (
+                  "Deploy Agent"
+                ) : (
+                  <>
+                    <VscLoading className="animate-spin" size={20} />
+                    <span className="ml-2">Running</span>
+                  </>
+                )}
+              </Button>
+
+              <Button
+                disabled={agent == null}
+                onClick={handleStopAgent}
+                className="mt-10"
+                enabledClassName={"bg-red-600 hover:bg-red-400"}
+              >
+                {stoppingAgent ? (
+                  <>
+                    <VscLoading className="animate-spin" size={20} />
+                    <span className="ml-2">Stopping</span>
+                  </>
+                ) : (
+                  "Stop agent"
+                )}
+              </Button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </DefaultLayout>
+  );
+};
+
+export default Home;
